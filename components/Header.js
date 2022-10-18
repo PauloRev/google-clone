d48073126099e0c479e39e5675bbb64e0f98d59c
PauloRev <paulohenriquerev@gmail.com>
2022-10-18 13:10:20 -0300

    Fix buttons and image

--- components/Header.js
+++ components/Header.js
@@ -5,15 +5,15 @@ export default function Header() {
   return (
     <header className="flex justify-between p-6 text-sm text-gray-700">
       <div className="flex space-x-4 items-center">
-        <Link href="https://about.google.com/">
+        <Link href="https://about.google.com">
           <a className="link">About</a>
         </Link>
-        <Link href="https://store.google.com/">
+        <Link href="https://store.google.com">
           <a className="link">Store</a>
         </Link>
       </div>
       <div className="flex space-x-4 items-center">
-        <Link href="https://mail.google.com/mail/&ogbl">
+        <Link href="https://mail.google.com/mail">
           <a className="link">Gmail</a>
         </Link>
         <Link href="https://github.com/PauloRev/google-clone">
--- components/User.js
+++ components/User.js
@@ -8,7 +8,7 @@ export default function User({ className }) {
       <>
         <img
           onClick={signOut}
-          src={session?.user?.image}
+          src={session.user?.image}
           alt="photo"
           className={`rounded-full h-10 w-10 shadow-sm hover:scale-105 transition-all delay-75 cursor-pointer hover:bg-gray-100 p-1 ${className}`}
         />
