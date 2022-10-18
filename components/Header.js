5b71fe0c06619b014044594411330c0159e00760
PauloRev <paulohenriquerev@gmail.com>
2022-10-18 13:05:08 -0300

    Fix header buttons

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,24 @@
+import Link from 'next/link';
 import User from './User';
 
 export default function Header() {
   return (
     <header className="flex justify-between p-6 text-sm text-gray-700">
       <div className="flex space-x-4 items-center">
-        <a
-          className="link text-sm no-underline hover:underline text-gray-700"
-          href="https://github.com/PauloRev/google-clone"
-          rel="noreferrer"
-        >
-          Github
-        </a>
+        <Link href="https://about.google.com/">
+          <a className="link">About</a>
+        </Link>
+        <Link href="https://store.google.com/">
+          <a className="link">Store</a>
+        </Link>
       </div>
       <div className="flex space-x-4 items-center">
-        <a
-          className="link text-sm no-underline hover:underline text-gray-700"
-          href="https://mail.google.com/mail/&ogbl"
-          rel="noreferrer"
-        >
-          Gmail
-        </a>
+        <Link href="https://mail.google.com/mail/&ogbl">
+          <a className="link">Gmail</a>
+        </Link>
+        <Link href="https://github.com/PauloRev/google-clone">
+          <a className="link">Github</a>
+        </Link>
         <User />
       </div>
     </header>
