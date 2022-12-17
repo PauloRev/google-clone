64279f82b77f99c709aaba93b0d46773325169a0
paulhdev <paulohenriquerev@gmail.com>
2022-12-17 16:43:54 -0300

    Change github username

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,9 @@ export default function Footer() {
   return (
     <footer className="w-full flex flex-col items-center justify-center mt-32 mb-2">
       <p className="text-gray-700">
-        Copyright &copy; {new Date().getFullYear()} - PhDev
+        Copyright &copy; {new Date().getFullYear()} - Paulo Henrique
       </p>
-      <Link href="https://github.com/PauloRev/google-clone">
+      <Link href="https://github.com/paulhdev/google-clone">
         <a className="link">Github Project</a>
       </Link>
     </footer>
